cf06b9c2b674858bfa2b9a76431a3a6612e7068b
notGonim <mhmodgonim96@gmail.com>
2021-04-19 11:07:58 +0200

    Just praatice Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { lorem } from 'faker';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  randomText = lorem.sentence();
+  solve = false;
+
+  onHandleInput(value: string) {
+    if (value === this.randomText) {
+      this.solve = true;
+    }
+  } 
+}
